6ab2b5ea5bba466ccf88587ac56a7ce3259d8a4f
sophxD <100835089+sophxD@users.noreply.github.com>
2022-10-13 08:33:15 +0200

    Update ResetButton.cs

--- Assets/MyGame/Scripts/ResetButton.cs
+++ Assets/MyGame/Scripts/ResetButton.cs
@@ -8,19 +8,6 @@ public class ResetButton : MonoBehaviour
 {
     public Button resetButton;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void SwitchScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
